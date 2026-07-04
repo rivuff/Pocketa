d930940ec0353472180aa690e027e0bdce5bed87
Rivu Naskar <rivunaskar0@gmail.com>
2026-07-05 01:20:36 +0530

    added some modifications

--- src/main/java/com/pocket/wallet/WalletApplication.java
+++ src/main/java/com/pocket/wallet/WalletApplication.java
@@ -4,11 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class WalletApplication {
 
 	public static void main(String[] args) {
+
+
+		Dotenv dotenv = Dotenv.load();
+
+
+		dotenv.entries().forEach(entry ->
+			System.setProperty(entry.getKey(), entry.getValue()));
+
+
 		SpringApplication.run(WalletApplication.class, args);
 	}
 
